23d50c5f9704e7de472e812154b0320451b59415
shreyaDeb <shreya.deb@icloud.com>
2023-10-30 12:25:25 -0600

    "day 1: sumo of 2 numbers"

---
+++ src/main/java/com/webtoom/day1/sum0f2numbers.java
@@ -0,0 +1,12 @@
+package com.webtoom.day1;
+
+public class sum0f2numbers
+{
+    public static void main(String [] args)
+    {
+        int a = 897428923;
+        int b = 389278239;
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
