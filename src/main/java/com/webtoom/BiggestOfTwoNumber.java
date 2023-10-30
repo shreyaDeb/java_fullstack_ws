9986a127c170cebb850acddc0bf907a093381aa1
shreyaDeb <shreya.deb@icloud.com>
2023-10-30 12:31:05 -0600

    "day 1: biggest of 2 numbers"

---
+++ src/main/java/com/webtoom/BiggestOfTwoNumber.java
@@ -0,0 +1,10 @@
+package com.webtoom;
+
+public class BiggestOfTwoNumber {
+    public static void main (String [] args)
+    {
+        int a = 123232421;
+        int b = 123282421;
+        System.out.println("Biggest is: " + (a > b ?  a : b));
+    }
+}
